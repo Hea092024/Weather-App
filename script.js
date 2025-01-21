061865a1d0b643035e0fecd98cb25517abab31d6
Hea092024 <Hea092024@gmail.com>
2025-01-21 13:32:50 +0100

    function now displays city + country on the website

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// async function getWeatherData(city) {
+//   const apiKey = "65b97b72fa58428d975121941252001";
+//   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+//   const result = await fetch(url);
+//   const data = await result.json();
+//   console.log(`City: ${data.location.name}`);
+//   console.log(`Country: ${data.location.country}`);
+// }
+
+// getWeatherData("Paris");
+
+
 async function getWeatherData(city) {
   const apiKey = "65b97b72fa58428d975121941252001";
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
   const result = await fetch(url);
   const data = await result.json();
-  console.log(`City: ${data.location.name}`);
-  console.log(`Country: ${data.location.country}`);
+
+  document.getElementById(
+    "location"
+  ).textContent = `${data.location.name}, ${data.location.country}`;
 }
 
-getWeatherData("Paris");
+document.getElementById("search-btn").addEventListener("click", function () {
+  const city = document.getElementById("city").value;
+  if (city) {
+    getWeatherData(city);
+  } else {
+    document.getElementById("location").textContent = "Please enter a city.";
+  }
+});
