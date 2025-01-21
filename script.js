fdbfc439e0bcbf2c988f8620b2d8012c1e8932b1
Hea092024 <Hea092024@gmail.com>
2025-01-21 11:57:18 +0100

    changed from http to https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeatherData(city) {
   const apiKey = "65b97b72fa58428d975121941252001";
-  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
   const result = await fetch(url);
   const data = await result.json();
