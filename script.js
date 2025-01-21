631b046774a74fbb35f5303d0c99c14727848065
Hea092024 <Hea092024@gmail.com>
2025-01-21 11:43:17 +0100

    got the async function to work in js

---
+++ script.js
@@ -0,0 +1,11 @@
+async function getWeatherData(city) {
+  const apiKey = "65b97b72fa58428d975121941252001";
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  const result = await fetch(url);
+  const data = await result.json();
+  console.log(`City: ${data.location.name}`);
+  console.log(`Country: ${data.location.country}`);
+}
+
+getWeatherData("Paris");
